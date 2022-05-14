7371bf694c84d349722d286d057b3c8546949753
xfrogz <renattoyague@gmail.com>
2022-05-14 17:22:56 -0500

    Add controller/module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ConfigModule } from '@nestjs/config';
 //Importar modulo para MongoDB
 import { MongooseModule } from '@nestjs/mongoose';
+
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { JwtModule } from '@nestjs/jwt';
+import { secret } from './utils/constants';
+import { join } from 'path/posix';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/Stream'),
+    ConfigModule.forRoot( { envFilePath: `${process.env.secrect}.env` }),
+    JwtModule.register({
+      secret,
+      signOptions: { expiresIn: '2h' },
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
